145fda5890c932c3cfe2563ae512910e1092eb92
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-02-04 12:21:58 +1300

    Changed function names to be more descriptive

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,32 @@
 const textEditor = document.getElementById("text-editor");
+const rightSplit = document.getElementById("right-split");
 
 const code = [
   [1, ["<h1>Portfolio</h1>"]],
   [2, ["<p>This is my portfolio</p>"]],
 ];
 
-// Returns an array of same size but second element is an array full of coloured characters
+let rawCodeProgress = "";
+
+// Returns an array of same size but second element is an array full of coloured characters and stripped htmlentities
 const colourCode = (code) => {};
 
-const displayHTML = (html) => {
+const displayCode = (html) => {
   textEditor.innerHTML += html;
 };
 
-const displayCode = (code) => {
-  code.forEach((line) => {
-    line[1].forEach((char) => {
-      displayHTML(char);
-    });
-  });
+const displayHTML = () => {
+  rightSplit.innerHTML = rawCodeProgress;
 };
 
-displayCode(code);
+const updateSite = (code) => {
+  code.forEach((line) => {
+    line[1].forEach((char) => {
+      rawCodeProgress += char;
+      displayCode(char);
+    });
+  });
+  displayHTML();
+};
+
+updateSite(code);
