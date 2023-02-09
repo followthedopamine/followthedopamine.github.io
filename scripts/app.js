a41b01393bf46b1d643b44a92923dc2a07396c62
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-02-09 15:13:02 +1300

    Added a better design for the right side

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,41 @@
 const textEditor = document.getElementById("text-editor");
 const rightSplit = document.getElementById("right-split");
 
-const code = `<div class="center">
-	<h1>Mark Jenkins</h1>
-	<h2>https://github.com/followthedopamine</h2>
-	<h3>followthedopamine@gmail.com</h3>
+const code = `<div class="center dark-bg">
+  <div class="container">
+    <h1>Mark Jenkins</h1>
+    <h2>https://github.com/followthedopamine</h2>
+    <h3>followthedopamine@gmail.com</h3>
+    <p>
+      I'm a passionate hobbyist programmer with expertise in competitive
+      programming, machine learning, and game development. Seeking to
+      make my mark in the thriving NZ video game industry, I'm looking
+      for entry-level opportunities to showcase my skills and drive.
+      Let's create something amazing together.
+    </p>
+    <div id="featured-thumbnails">
+      <span class="col-1">
+        <img class="large-thumbnail" src="/img/test.png" />
+      </span>
+    </div>
+    <div id="thumbnails">
+      <div class="col-3">
+        <img
+          class="medium-thumbnail left-thumbnail"
+          src="/img/test.png"
+        />
+      </div>
+      <div class="col-3">
+        <img class="medium-thumbnail" src="/img/test.png" />
+      </div>
+      <div class="col-3">
+        <img
+          class="medium-thumbnail right-thumbnail"
+          src="/img/test.png"
+        />
+      </div>
+    </div>
+  </div>
 </div>`;
 let rawCodeProgress = "";
 let charCount = 0;
@@ -66,7 +97,7 @@ const updateSite = () => {
 };
 
 const startTyping = (code) => {
-  setInterval(updateSite, 500);
+  setInterval(updateSite, 20);
 };
 
 startTyping(code);
