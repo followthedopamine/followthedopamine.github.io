33ca47ea68251897c2c06b4419b736144b442656
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-02-09 15:36:39 +1300

    Added line numbers

--- scripts/app.js
+++ scripts/app.js
@@ -1,51 +1,55 @@
 const textEditor = document.getElementById("text-editor");
 const rightSplit = document.getElementById("right-split");
 
-const code = `<div class="center dark-bg">
-	<div class="container">
-		<h1>Mark Jenkins</h1>
-		<h2>https://github.com/followthedopamine</h2>
-		<h3>followthedopamine@gmail.com</h3>
-		<p>
-			I'm a passionate hobbyist programmer with expertise in competitive
-			programming, machine learning, and game development. Seeking to
-			make my mark in the thriving NZ video game industry, I'm looking
-			for entry-level opportunities to showcase my skills and drive.
-			Let's create something amazing together.
-		</p>
-		<div id="featured-thumbnails">
-			<span class="col-1">
-				<img class="large-thumbnail" src="/img/test.png" />
-			</span>
-		</div>
-		<div id="thumbnails">
-			<div class="col-3">
-				<img
-					class="medium-thumbnail left-thumbnail"
-					src="/img/test.png"
-				/>
+const code = `		<div class="center dark-bg">
+			<div class="container">
+				<h1>Mark Jenkins</h1>
+				<h2>https://github.com/followthedopamine</h2>
+				<h3>followthedopamine@gmail.com</h3>
+				<p>
+					I'm a passionate hobbyist programmer with expertise in competitive
+					programming, machine learning, and game development. Seeking to
+					make my mark in the thriving NZ video game industry, I'm looking
+					for entry-level opportunities to showcase my skills and drive.
+					Let's create something amazing together.
+				</p>
+				<div id="featured-thumbnails">
+					<span class="col-1">
+						<img class="large-thumbnail" src="/img/test.png" />
+					</span>
+				</div>
+				<div id="thumbnails">
+					<div class="col-3">
+						<img
+							class="medium-thumbnail left-thumbnail"
+							src="/img/test.png"
+						/>
+					</div>
+					<div class="col-3">
+						<img class="medium-thumbnail" src="/img/test.png" />
+					</div>
+					<div class="col-3">
+						<img
+							class="medium-thumbnail right-thumbnail"
+							src="/img/test.png"
+						/>
+					</div>
+				</div>
 			</div>
-			<div class="col-3">
-				<img class="medium-thumbnail" src="/img/test.png" />
-			</div>
-			<div class="col-3">
-				<img
-					class="medium-thumbnail right-thumbnail"
-					src="/img/test.png"
-				/>
-			</div>
-		</div>
-	</div>
-</div>`;
+		</div>`;
 let rawCodeProgress = "";
 let charCount = 0;
 let colour = "red";
+let lineNumber = 1;
 
 // Returns an array of same size but second element is an array full of coloured characters and stripped htmlentities
 const colourCode = (code) => {};
 
 const displayCode = (char) => {
-  if (char === "\n") char = "<br />";
+  if (char === "\n") {
+    char = "<br />" + addColour(lineNumber, "gray");
+    lineNumber++;
+  }
   if (char === "\t") char = "&emsp;";
   if (char === "<") {
     colour = "gray";
@@ -75,6 +79,11 @@ const displayCode = (char) => {
     colour = "orange";
   }
   char = addColour(char, colour);
+
+  if (charCount == 0) {
+    char = addColour(lineNumber, "gray") + char;
+    lineNumber++;
+  }
   textEditor.innerHTML += char;
 };
 
